009d1e1cc92320f2c8a2538c532bb3820b81a7b8
Tim Kolar <tkolar@pobox.com>
2021-04-19 09:42:30 -0500

    Checkpoint

--- RMDataAnalysis/circulation/not_returned.py
+++ RMDataAnalysis/circulation/not_returned.py
@@ -11,9 +11,26 @@ from rmd_common import rmd_get_circulation
 
 def main():
 
+    march_first = datetime.date(2021, 3, 1)
+
     items = rmd_get_items()
     circulation = rmd_get_circulation()
 
+    for day in circulation:
+        circs = circulation[day]
+        for entry in circs:
+            #
+            # If it's recent, skip it.
+            #
+            mstr, dstr, ystr = circs[entry]["Expected Back"].split("/")
+            day = datetime.date(month=int(mstr), day=int(dstr), year=int(ystr))
+            if day > march_first:
+                continue
+
+            if circs[entry]["Check In"] == "":
+                print(circs[entry])
+
+    sys.exit()
     p = figure(title="Checkouts per day", 
                 x_axis_label="Date", x_axis_type='datetime',
                 y_axis_label='y',
@@ -29,9 +46,11 @@ def main():
         y.append(len(circulation[key]))
 
     smooth_y = np.convolve(y, np.ones(30)/30, mode='valid')
+
     p.line(x, smooth_y, legend_label="Days", line_width=2)
 
     show(p)
+
 
 main()
 
--- RMDataAnalysis/rmd_common.py
+++ RMDataAnalysis/rmd_common.py
@@ -1,8 +1,8 @@
 import sys
 import csv
-from datetime import datetime
+import datetime
 
-ROOTDIR="/Users/tkolar/mel_inventory/inventory_2021/ResourceMateData/" 
+ROOTDIR="/Users/tkolar/mel_inventory/data_analysis/RMDataAnalysis/" 
 
 #
 # items.csv fields
@@ -33,7 +33,7 @@ def rmd_get_circulation():
         checkout = row["Check Out"]
         barcode = row["Barcode"]
         month, day, year = checkout.split('/')
-        day = datetime(int(year), int(month), int(day))
+        day = datetime.datetime(int(year), int(month), int(day))
         if day not in circulation:
             circulation[day] = {}
         circulation[day][barcode] = row
