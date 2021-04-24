8e4a450858c2c9fab4e983e4a069ff6ca68bab5a
Tim Kolar <tkolar@Tims-MacBook-Pro.local>
2021-04-24 16:00:49 -0500

    closer to the great ISBN dump

--- RMDataAnalysis/scripts/i.py
+++ RMDataAnalysis/scripts/i.py
@@ -5,13 +5,15 @@ def main():
     isbns = file_text.split("\n")
     isbns = isbns[:-1]
 
-    isbns = ["0006392393"]
     for isbn in isbns:
         outfile = open("isbns/" + isbn, "w")
         url = "http://openlibrary.org/isbn/" + isbn + ".json"
+        print(url)
         r = requests.get(url)
-        print(r)
-        outfile.write(r.text)
+        if r.status_code != 200:
+            outfile.write("{\"status_code\": \"%d\"}" % r.status_code)
+        else:
+            outfile.write(r.text)
 
 
 main()
